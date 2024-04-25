009295ec280d227e684905887727c424eb4f8cd0
dehanj <jobson@tillitis.se>
2024-04-25 09:00:24 +0200

    Day 4 part 2

--- day4/main.go
+++ day4/main.go
@@ -6,13 +6,20 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
 // Idea
 // Split in winning numbers and cards
 // Use regex with groups, check if each winning card exist in my
-// cards
+// cards, sum.
+
+// Part 2
+// add a map with values of orignals + copies.
+// Sum the map in the end.
+
+var copies = map[int]int{}
 
 func main() {
 
@@ -25,16 +32,18 @@ func main() {
 	scanner.Split(bufio.ScanLines)
 
 	re := regexp.MustCompile("([0-9]+)")
-
+	var gameId int
 	total := 0
+
 	for scanner.Scan() {
 		round := 0
 		line := scanner.Bytes()
-		fmt.Printf("line: %s\n", line)
+		fmt.Printf("\nline: %s\n", line)
 		split := bytes.Split(line, []byte("|"))
 
 		winning := re.FindAll(split[0], -1)
 		cards := re.FindAll(split[1], -1)
+		gameId, _ = strconv.Atoi(string(winning[0]))
 		winning = winning[1:]
 		fmt.Printf("%s\n", winning)
 		fmt.Printf("%s\n", cards)
@@ -42,15 +51,35 @@ func main() {
 		for _, k := range winning {
 			for _, y := range cards {
 				if bytes.Equal(k, y) {
-					if round == 0 {
-						round = 1
-					} else {
-						round *= 2
-					}
+					round++
+
+					// Part 1
+					// 	if round == 0 {
+					// 		round = 1
+					// 	} else {
+					// 		round *= 2
+					// 	}
 				}
 			}
 		}
-		total += round
-		fmt.Printf("total: %v\n", total)
+
+		fmt.Printf("round: %d\n", round)
+		// Part 1
+		// total += round
+		// fmt.Printf("total: %v\n", total)
+
+		copies[gameId] = copies[gameId] + 1 // Add the original one
+
+		// Add copies to list
+		for i := 1; i <= round; i++ {
+			copies[gameId+i] = copies[gameId+i] + copies[gameId]
+		}
+
+		fmt.Printf("copies: %v\n", copies)
+
 	}
+	for l := range copies {
+		total += copies[l]
+	}
+	fmt.Printf("total: %v\n", total)
 }
